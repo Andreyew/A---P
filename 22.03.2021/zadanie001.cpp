a4ce4ad7838686123c63fe894a180212c4e7adab
pdamaszk <pdamaszk@gmail.com>
2021-03-22 23:29:12 +0100

    choinka

--- 22.03.2021/zadanie001.cpp
+++ 22.03.2021/zadanie001.cpp
@@ -11,3 +11,32 @@
 //   #########
 //  ###########
 // #############
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int width = 13,
+        height = 10;
+
+    char space = ' ',
+         point = '#';
+
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = 1; j <= width; j++)
+        {
+            // if (j == width / 2 + 1)
+            //     cout << point;
+            if (j >= (width / 2 + 1) - (i - 1) && j <= (width / 2 + 1) + (i - 1) && i <= 4)
+                cout << point;
+            else if (j >= (width / 2 + 1) - (i - 4) && j <= (width / 2 + 1) + (i - 4) && i > 4)
+                cout << point;
+            else
+                cout << space;
+        }
+        cout << endl;
+    }
+}
